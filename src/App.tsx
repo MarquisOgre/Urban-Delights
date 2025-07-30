dbb102ad1a03e4cb7e9dc978f4aaab292c4666bd
MarquisOgre <marquisogre@gmail.com>
2025-07-30 04:51:02 +0000

    Refactor Stock Register Component
    Move the Stock Register code into its own component to improve navigation and code organization.

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RecipesDashboard from "./pages/RecipesDashboard";
-import StockRegister from "./pages/StockRegister";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -17,7 +16,6 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<RecipesDashboard />} />
-          <Route path="/stock-register" element={<StockRegister />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
