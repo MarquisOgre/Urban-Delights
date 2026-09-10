964b42965fca1d3cd7491cbf2f141ac8bf1e8ed1
Marquis Ogre <marquisogre@gmail.com>
2026-09-10 12:46:39 +0530

    Route Admin Store Manager as the storefront CMS

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import NotFound from "./pages/NotFound";
 import Auth from "./pages/Auth";
 import Store from "./pages/Store";
 import LegalPage from "./pages/LegalPage";
-import Settings from "./pages/Settings";
+import StoreManager from "./pages/StoreManager";
 import { AuthProvider, useAuth } from "@/hooks/use-auth";
 
 const queryClient = new QueryClient();
@@ -49,7 +49,7 @@ const App = () => (
             <Route path="/login" element={<Auth />} />
             <Route path="/auth" element={<Navigate to="/login" replace />} />
             <Route path="/admin" element={<AdminRoute><BackendDashboard /></AdminRoute>} />
-            <Route path="/admin/settings" element={<AdminRoute><Settings /></AdminRoute>} />
+            <Route path="/admin/settings" element={<AdminRoute><StoreManager /></AdminRoute>} />
             <Route path="/privacy-policy" element={<LegalPage />} />
             <Route path="/terms-and-conditions" element={<LegalPage />} />
             <Route path="/shipping-policy" element={<LegalPage />} />
