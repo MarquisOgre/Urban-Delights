4b87211642ed5bdbd128dfd988c56e7c40830538
MarquisOgre <marquisogre@gmail.com>
2026-09-11 09:47:10 +0530

    Load mobile checkout scroll styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import './checkout-compact.css'
+import './checkout-mobile-scroll.css'
 
 createRoot(document.getElementById("root")!).render(<App />);
