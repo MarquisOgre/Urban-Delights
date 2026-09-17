5b2982668b86a65da55471ed1f4c860f24105fa4
Marquis Ogre <marquisogre@gmail.com>
2026-09-18 00:34:37 +0530

    Apply unified public header layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import './header-nav.css'
 import './checkout-compact.css'
 import './checkout-mobile-scroll.css'
 import './checkout-footer.css'
